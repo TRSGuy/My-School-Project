20fea559c9fa1f24d0400346ba92b2865e74fb6b
TRSGuy <kimpankarl@gmail.com>
2017-05-22 21:28:47 +0200

    Added a bunch of new things. Revamped the way the site content was generated

--- js/main.js
+++ js/main.js
@@ -1,128 +1,57 @@
 content = {
-  'ED16': [
-    {
-      'name': 'Navigation',
-      'type': 'main-header',
-      'classes': ['main-header', 'list-divider'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Upcomming',
-      'type': 'list-header',
-      'classes': ['list-header', 'list-divider'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Schedule',
-      'type': 'external_link',
-      'url': 'http://voraschem.tk',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Information',
-      'type': 'list-header',
-      'classes': ['list-header', 'list-divider'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'School Information',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Upcomming Events',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'People',
-      'type': 'list-header',
-      'classes': ['list-header', 'list-divider'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Classmates',
-      'type': 'list',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Teachers',
-      'type': 'list',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-
-    {
-      'name': 'Subjects',
-      'type': 'list-header',
-      'classes': ['list-header', 'list-divider'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Web Development',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Computer Science',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Mechatronics',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Electromechanics',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Math',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'English',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    },
-    {
-      'name': 'Swedish',
-      'type': 'content',
-      'classes': ['list-item'],
-      'element': '<li/>',
-      'parent_id': 'sidebar'
-    }
-  ]
+  'Upcomming Events': {
+    'header': 'Upcomming events',
+    'content-type': 'text',
+    'content': 'There is currently nothing scheduled for the rest of this week!'
+  },
+  'Classmates': {
+    'header': 'Classmates',
+    'content-type':'name-list',
+    'content': [['John', 'Doe'], ['John', 'Doe II'], ['John', 'Doe III'], ['John', 'Doe IV']]
+  },
+  'Teachers': {
+    'header': 'Teachers',
+    'content-type':'contact-list',
+    'content': [
+      {
+        'subject': 'Computer Science',
+        'teacher-name': ['Vahap', 'Bilgec'],
+        'picture-url': 'https://schoolsoftelev.nacka.se/nacka/jsp/student/pictureFile.jsp?teacherId=2037'
+      },
+      {
+        'subject': 'Web Development',
+        'teacher-name': ['Garabed', 'Hakopian'],
+        'picture-url': 'https://schoolsoftelev.nacka.se/nacka/jsp/student/pictureFile.jsp?teacherId=2047'
+      },
+      {
+        'subject': 'Mechatronics',
+        'teacher-name': ['Shirin', 'Nasirpour'],
+        'picture-url':'https://schoolsoftelev.nacka.se/nacka/jsp/student/pictureFile.jsp?teacherId=4343'
+      },
+      {
+        'subject': 'Electromechanics',
+        'teacher-name': ['Kjell', 'Nyberg'],
+        'picture-url': false // All false should be replaced with http://www.shawnee.edu/_resources/images/profile-placeholder.png
+      },
+      {
+        'subject': 'English',
+        'teacher-name': ['Catharina', 'Winbäck'],
+        'picture-url': 'https://schoolsoftelev.nacka.se/nacka/jsp/student/pictureFile.jsp?teacherId=2081'
+      },
+      {
+        'subject': 'Math',
+        'teacher-name': ['Anna', 'Natanaelsson'],
+        'picture-url': false
+      },
+      {
+        'subject': 'Swedish',
+        'teacher-name': ['Åse', 'Ståhlberg'],
+        'picture-url': false
+      }
+    ]
+  }
 }
+
 console.log(content);
 function time() {
     var now = new Date();
@@ -140,53 +69,81 @@ function time() {
 window.onload = main();
 window.onresize = function(event) {
   if (document.cookie !== "") {
-    get_schedule();
+    get_schedule(true);
   }
 };
 
+function change_content(obj) {
+  var name = obj.innerHTML;
+  console.log(content)
+  $('#main-content').empty();
+  $('.main-content-header').text(name);
+
+  if(content[name]['content-type'] == 'text') {
+    page_content = $('<div>', {text: content[name]['content']})
+    $('#main-content').append(page_content);
+  }
+
+  else if(content[name]['content-type'] == 'name-list') {
+    page_content = $('<ul>', {class: 'name-list'});
+    for (var i = 0; i < content[name]['content'].length; i++) {
+      first = $('<span>', {class: 'first', 'text': content[name]['content'][i][0] + ' '});
+      last = $('<span>', {class: 'last', 'text': content[name]['content'][i][1]});
+      list_element = $('<li>', {class: 'name'});
+      list_element.append(first);
+      list_element.append(last);
+      page_content.append(list_element);
+      $('#main-content').append(page_content);
+    }
+  }
+
+  else if (content[name]['content-type'] == 'contact-list') {
+    list_root = $('<ul>', {class: 'contact-list'});
+    for (var i = 0; i < content[name]['content'].length; i++) {
+      list_root = $('<ul>', {class: 'contact-entry'});
+      name = $('<li>', {class: 'name'});
+      first = $('<span>', {class: 'first', 'text': content[name]['content'][i]['teacher-name'][0] + ' '});
+      last = $('<span>', {class: 'last', 'text': content[name]['content'][i]['teacher-name'][1]});
+      name.append(first);
+      name.append(last);
+    }
+  }
+
 function main() {
+  $('.navigation-item').each(
+    function() {
+      $(this).attr('onclick', 'change_content(this);')
+    }
+  )
   if (document.cookie !== "") {
-    initialize(false);
+    initialize(true);
   }
 }
-
-
 
 function get_schedule() {
   var time_array = time();
   var w = Math.round($('.schedule').width());
   var h = Math.round($('.schedule').height());
-  var id = document.cookie.split('=')[1]
+  var id = $('#class-id').val();
   var url = 'http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=58700/sv-se&type=-1&id=' + id + '&period=&week=' + time_array[0] + '&mode=0&printer=0&colors=32&head=1&clock=1&foot=1&day=' + time_array[1] + '&width=' + w + '&height=' + h + '&maxwidth=' + w + '&maxheight=' + h + '';
-  $('#schedule').attr('src', url);
+  $('#schedule-image').attr('src', url);
+  console.log(url)
 }
 
-function initialize(store_cookie) {
-    if(store_cookie) {
-      var id = $('#class-id').val();
-      document.cookie = "id=" + id;
-      $('.id').slideUp("100");
-      $('.grid').slideDown("100");
-    }
-    else {
-      var id = document.cookie.split('=')[1]
-      $('.grid').show();
-      $('.id').slideUp();
-    }
-    if(id in content) {
-      get_schedule();
-      for (var i = 0; i < content[id].length; i++) {
-        tmp_item = content[id][i]
-        item_text = ''
-        base_element = ''
-        class_array = tmp_item['classes'];
-        class_string = ''
-        for (var i = 0; i < class_array.length; i++) {
-          class_string += class_array[i];
-        }
-      }
+function initialize(cookie_exists) {
+  if(cookie_exists) {
+    var id = document.cookie.split('=')[1];
+    $('.id').hide();
   }
   else {
-    console.log("Sorry, this id doesn't exist in our database, are you sure you spelled it correctly?")
+    var id = $('#class-id').val();
+    document.cookie = 'classid=' + id;
   }
+  if(id !== '') {
+    $('.grid').show();
+    $('.id').slideUp();
+    document.cookie = 'classid=' + id;
+    get_schedule();
+  }
+  // TODO: ADD AN ELSE TO PROMPT THE USER ABOUT THE ID BEING INVALID
 }
